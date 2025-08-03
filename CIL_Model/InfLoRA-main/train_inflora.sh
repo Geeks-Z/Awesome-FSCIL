d59e36589ded907ca22809c9e736722ee319aaeb
Geeks_Z <izhw1024@163.com>
2025-08-03 09:23:18 +0800

    新增脚本

--- CIL_Model/InfLoRA-main/train_inflora.sh
+++ CIL_Model/InfLoRA-main/train_inflora.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-python main.py --config configs/inflora_cub_B100_Inc10.json
+#python main.py --config configs/inflora_cub_B100_Inc10.json
 python main.py --config configs/inflora_cifar_B60_Inc5.json
 python main.py --config configs/inflora_mini_B60_Inc5.json
-python main.py --config configs/inflora_inr_B100_Inc10.json
+#python main.py --config configs/inflora_inr_B100_Inc10.json
 
--- CIL_Model/SD-Lora-CL-main/train_sdlora.sh
+++ CIL_Model/SD-Lora-CL-main/train_sdlora.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python main.py --config=./exps/sdlora_cub.json
+#python main.py --config=./exps/sdlora_cub.json
 python main.py --config=./exps/sdlora_c100.json
 python main.py --config=./exps/sdlora_mini.json
-python main.py --config=./exps/sdlora_inr.json
+#python main.py --config=./exps/sdlora_inr.json
