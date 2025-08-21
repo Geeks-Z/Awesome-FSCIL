432486adc1a7ca13469cbeacd5c0726f79a66e8b
hwzhao <izhw1024@163.com>
2025-08-21 13:18:09 +0800

    简化脚本

--- SEC-Prompt-main/常用命令.sh
+++ SEC-Prompt-main/常用命令.sh
@@ -7,14 +7,3 @@ conda activate peft
 ## 单卡
 
 nohup ./train.sh > ../results/SEC-Prompt-Matrix.out 2>&1 &
-
----------------------------------------------------------------------------------------------------------------
-
-## 多卡
-nohup ./ddp_train_B0.sh > ./results/DLEPEM-B0-multiGPU-inr.out 2>&1 &
-
-
-
-
-nohup ./loop_B0.sh > ./results/loop_B0.out 2>&1 &
-nohup ./loop_B50.sh > ./results/loop_B50.out 2>&1 &
